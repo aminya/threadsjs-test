dcd8f8617e62c123fa44bed43f03ee2d5391734c
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-08-16 21:48:16 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
-console.log(`trueness: ${process.env.secretVar_Macro == "1234"}`)
+console.log(`trueness: ${indexof(process.env.secretVar_Macro, "1234")}`)
 console.log(`node: ${process.env.secrectVar_Macro}`)
